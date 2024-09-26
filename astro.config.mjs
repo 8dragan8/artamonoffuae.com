bf84bb1198fc52931642bc86b4d6f71ca2e010f0
8dragan8 <35200825+8dragan8@users.noreply.github.com>
2024-09-26 23:08:24 +0200

    added icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';
-import vercelStatic from '@astrojs/vercel/static';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
+import vercelStatic from '@astrojs/vercel/static'
+import { defineConfig } from 'astro/config'
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
   // Must be 'static' or 'hybrid'
   output: 'static',
   adapter: vercelStatic(),
-  integrations: [tailwind()]
-});
+  integrations: [icon(), tailwind()],
+})
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			backgroundImage: {
-				'hero-image': 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), var(--backgroundUrlProp)',
-				'section-image': 'var(--backgroundUrlProp)',
-			}
-		},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-image': 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), var(--backgroundUrlProp)',
+        'section-image': 'var(--backgroundUrlProp)',
+      },
+    },
+  },
+  plugins: [],
 }
