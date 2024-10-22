e970863e6e3ad044e5d667edfb58d195657d3a54
8dragan8 <35200825+8dragan8@users.noreply.github.com>
2024-10-22 22:41:43 +0200

    add thankyou section

--- src/content/types.ts
+++ src/content/types.ts
@@ -36,62 +36,9 @@ export interface Section {
   description: string
   items: SectionItem[]
 }
-export interface Section1 {
-  title: string
-  subtitle: string
-  items: Section1Item[]
-}
 
 export interface SectionItem {
   title?: string
   icon?: string
   keys?: string[]
-}
-export interface Section1Item {
-  title: string
-  icon: string
-  keys: string[]
-}
-
-export interface Section2 {
-  title: string
-  items: Section2Item[]
-}
-
-export interface Section2Item {
-  title: string
-  keys: string[]
-}
-
-export interface Section3 {
-  title: string
-  subtitle: string
-  description: string
-  items: Section3Item[]
-}
-
-export interface Section3Item {
-  icon: string
-  keys: string[]
-}
-
-export interface Section4 {
-  title: string
-  subtitle: string
-  items: Section4Item[]
-}
-
-export interface Section4Item {
-  title: string
-  subtitle?: string
-  keys: string[]
-}
-
-export interface Section5 {
-  title: string
-  items: Section5Item[]
-}
-
-export interface Section5Item {
-  keys: string[]
 }
