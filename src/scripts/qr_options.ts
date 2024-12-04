8e12a10a0944d905b767974db12c776a1d6e3a55
8dragan8 <35200825+8dragan8@users.noreply.github.com>
2024-12-04 13:10:30 +0100

    added qr code in contact

---
+++ src/scripts/qr_options.ts
@@ -0,0 +1,42 @@
+import QRCodeStyling, { type Options } from 'qr-code-styling'
+
+const qr_options: Options = {
+  type: 'svg',
+  shape: 'square',
+  width: 200,
+  height: 200,
+  data: 'https://www.artamonoffuae.com/',
+  margin: 0,
+  qrOptions: {
+    typeNumber: 0,
+    mode: 'Byte',
+    errorCorrectionLevel: 'Q',
+  },
+  imageOptions: {
+    saveAsBlob: true,
+    hideBackgroundDots: true,
+    imageSize: 0.5,
+    margin: 5,
+  },
+  dotsOptions: {
+    type: 'extra-rounded',
+    color: '--clr-primary',
+    roundSize: true,
+    gradient: undefined,
+  },
+  backgroundOptions: {
+    round: 0,
+    color: 'white',
+  },
+  cornersSquareOptions: {
+    type: 'extra-rounded',
+    color: '--clr-primary',
+    gradient: undefined,
+  },
+  cornersDotOptions: {
+    type: 'dot',
+    color: '--clr-primary',
+  },
+}
+
+export default qr_options
